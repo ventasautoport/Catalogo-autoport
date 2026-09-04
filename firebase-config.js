75137f654401111a9c985c9f49b03c3ef76f9c08
Autoport SRL Automatismos y Seguridad <ventas.autoport@gmail.com>
2026-09-04 19:35:32 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import Firebase SDKs from CDN for browser usage
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCejLcaSfJTa_yYPnbZsAZterxI46sy4m0",
+  apiKey: "AIzaSyCeJlcaSfJTa_yPNbZsAZterxI46sy4m0",
   authDomain: "catalogo-autoport.firebaseapp.com",
   projectId: "catalogo-autoport",
   storageBucket: "catalogo-autoport.firebasestorage.app",
@@ -15,3 +12,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export { app };
